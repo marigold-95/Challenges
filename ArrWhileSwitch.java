41f03ed8ab69358cd762817424b8a7f6b700fc94
marigold-95 <97016865+marigold-95@users.noreply.github.com>
2022-01-05 18:46:50 +0200

    Add files via upload

---
+++ ArrWhileSwitch.java
@@ -0,0 +1,35 @@
+
+
+public class ArrWhileSwitch {
+    public static void main(String[] args){
+        int[] arr = new int[50];
+        System.out.println("Generated array ");
+        for(int n=0;n<arr.length;n++){
+            arr[n] = (int)(Math.random()*50);
+            System.out.print(arr[n]+",");
+        }
+        System.out.println();
+        System.out.println("Even numbers :");
+        int i=0;
+        while(i<arr.length){
+            switch (arr[i]%2) {
+                case 0:
+                System.out.print(arr[i] + ", ");
+            }
+            i++;
+        }
+        System.out.println();
+        System.out.println("Multiples of 11 :");
+        int j=0;
+        while( j<arr.length){
+            switch(arr[j]%11 ) {
+                case 0:
+                System.out.print(arr[j] + ", ");
+            }
+            j++;
+        }
+    }
+
+}
+
+
